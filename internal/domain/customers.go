5dde7d275848d1e0851a92b5e220c100323382c1
Ricardo Emmanuel <remm@Ricardos-MacBook-Pro.local>
2026-08-16 18:19:58 -0600

    feat(customer): add before create method to set the uuid

--- internal/domain/customers.go
+++ internal/domain/customers.go
@@ -3,6 +3,7 @@ package domain
 import (
 	"github.com/google/uuid"
 	"github.com/shopspring/decimal"
+	"gorm.io/gorm"
 )
 
 type Customer struct {
@@ -10,4 +11,11 @@ type Customer struct {
 	FullName string          `gorm:"size:75;not null" json:"full_name"`
 	Phone    string          `gorm:"type:varchar(20);not null;uniqueIndex" json:"phone"`
 	Price    decimal.Decimal `gorm:"type:decimal(10,2);not null" json:"price"`
+}
+
+func (c *Customer) BeforeCreate(tx *gorm.DB) error {
+	if c.ID == uuid.Nil {
+		c.ID = uuid.New()
+	}
+	return nil
 }
